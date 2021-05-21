f9d80ad8c2f187c914ea5899e423ebf6cbd91ceb
Gordon Hodanich <ghodanich@gmail.com>
2021-05-21 16:43:40 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(PickupLocation){
+    let hqLocation = 42
+    if (PickupLocation >= hqLocation){
+        return PickupLocation - hqLocation
+    } else {
+        return hqLocation - PickupLocation
+    }
+}
+
+function distanceFromHqInFeet(PickupLocation){
+    let lengthOfBlock = 264
+    let disInFeet = distanceFromHqInBlocks(PickupLocation) * lengthOfBlock
+    return disInFeet
+}
+
+function distanceTravelledInFeet(startingLocation1, startingLocation2){
+    let lengthOfBlock = 264
+    if (startingLocation1 > startingLocation2){
+        return (startingLocation1 - startingLocation2) * lengthOfBlock
+    } else {
+        return (startingLocation2 - startingLocation1) * lengthOfBlock
+    }
+}
+
+function calculatesFarePrice(startingLocation1, startingLocation2){
+    let farePrice
+    if (distanceTravelledInFeet(startingLocation1, startingLocation2) <= 400){
+        return 0
+    }else if (distanceTravelledInFeet(startingLocation1, startingLocation2) > 400 && distanceTravelledInFeet(startingLocation1, startingLocation2) <= 1999){
+        return ((distanceTravelledInFeet(startingLocation1, startingLocation2)) - 400) * 0.02
+    }else if (distanceTravelledInFeet(startingLocation1, startingLocation2) > 2000 && distanceTravelledInFeet(startingLocation1, startingLocation2) <= 2500){
+        return 25
+    }else if (distanceTravelledInFeet(startingLocation1, startingLocation2) > 2500) {
+        return 'cannot travel that far'
+    }
+}
